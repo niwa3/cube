31c90864758e5730ab8dd7617425dcd2714951de
Akira NIWA <niwa@plant.westlab>
2016-07-11 18:32:27 +0900

    fixed

--- alu.v
+++ alu.v
@@ -51,11 +51,11 @@ always @(*) begin
 			zf <= 0;
 		end
 		R_90:begin //90度回転方向は自分で指定
-			if(in0==8'b00000000)begin
+			if(in0==8'b00000000)begin//x
 				out <= {in1[7:2],(in1[1:0]==2'b10)?in1[1:0]:(in1[1:0]~^2'b00)};
-			end else if(in0==8'b00000001)begin
+			end else if(in0==8'b00000001)begin//y
 				out <= {in1[7:2],(in1[1:0]==2'b11)?in1[1:0]:(in1[1:0]~^2'b01)};
-			end else if(in0==8'b00000010)begin
+			end else if(in0==8'b00000010)begin//z
 				out <= {in1[7:2],(in1[1:0]==2'b00)?in1[1:0]:(in1[1:0]~^2'b10)};
 			end else begin
 				out<=in1;
--- test_cube.v
+++ test_cube.v
@@ -10,9 +10,9 @@ module test;
 	always #0.5 clk = ~clk;
 
 	initial begin
-//		$dumpfile("test_cube.vcd");
-//		$dumpvars(0, t0);
-//		$dumplimit(1000000);
+		$dumpfile("test_cube.vcd");
+		$dumpvars(0, t0);
+		$dumplimit(1000000);
 		$monitor($stime,,"mem20:%d mem21:%d mem22:%d mem23:%d mem24:%d mem25:%d mem26:%d mem27:%d mem28:%d mem29:%d mem30:%d", mem20,mem21,mem22,mem23,mem24,mem25,mem26,mem27,mem28,mem29,mem30);
 		clk=0;
 		rst_n=1;
